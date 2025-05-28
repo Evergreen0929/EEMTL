a3614895bb5a1a1525c3d834a56e30fb70052afb
JingdongZhang <90333984+Evergreen0929@users.noreply.github.com>
2025-05-27 23:39:03 -0700

    Update run.sh

--- BridgeNet/run.sh
+++ BridgeNet/run.sh
@@ -3,7 +3,7 @@ CUDA_VISIBLE_DEVICES=0,1 python -m torch.distributed.launch \
 --nproc_per_node=2  \
 --use_env \
 main.py \
---config_exp './configs/pascal/pascal_dinov2G_bridgenet.yml' \
+--config_exp './configs/pascal/pascal_vitL_bridgenet.yml' \
 --run_mode train \
 --dataset_log 'PASCAL' \
 #--trained_model  pretrained_model_path
